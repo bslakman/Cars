5a8d6bd5a22b3f4021f840f95534218e3c3bf56b
Belinda Slakman <slakman.b@husky.neu.edu>
2017-01-31 15:43:19 -0500

    Update Edmunds notebook with code for getting pricing data
    And some comments/headings.

--- Edmunds.py
+++ Edmunds.py
@@ -1,35 +1,80 @@
 
 # coding: utf-8
 
-# In[38]:
+# # Accessing data from Edmunds API
+
+# Imports
+
+# In[120]:
 
 import requests
 import json
 
 
-# In[45]:
+# ## Get all the data on car makes
+
+# In[50]:
 
 f = open('api.txt', 'r')
 key = f.read()
 f.close()
 parameters = {'api_key': key}
-response = requests.get("https://api.edmunds.com/api/vehicle/v2/:makes?", params=parameters)
+response = requests.get("https://api.edmunds.com/api/vehicle/v2/makes", params=parameters)
 data = response.json()
 
 
-# In[46]:
+# In[51]:
 
-nice_name = data['status']
-parameters = {'makeNiceName': nice_name, 'year': 2012, 'fmt': json}
-civic_data = requests.get("https://api.edmunds.com/api/vehicle/v2/:makeNiceName?api_key=2e7hjvffdg4x4mvrw2vtgvad", params=parameters).json()
+#print(data)
 
 
-# In[47]:
+# ### Get all the style data for a particular make/model/year
 
-print(civic_data)
+# In[113]:
+
+make_nice_name = 'ford'
+model_nice_name = 'focus'
+year = '2012'
+
+
+# In[114]:
+
+parameters = {'fmt': 'json', 'api_key': key}
+model_data = requests.get("https://api.edmunds.com/api/vehicle/v2/{0}/{1}/{2}/styles".format(make_nice_name,model_nice_name,year), params=parameters).json()
+#print(civic_data)
 
 
 # craigslist price - kelly blue book value = dealer price - what they'll give you for it (percentage). give a range of uncertainties...
+
+# In[116]:
+
+for x in model_data['styles']:
+    if x['submodel']['body']=='Sedan': print(x['name'].encode('utf-8'))
+
+
+# ## Get price data for a particular style of that vehicle in a given zip code
+
+# In[117]:
+
+style = model_data['styles'][0] # chose one randomly
+print(style)
+styleid = style['id']
+zip_code = '02143'
+
+
+# In[118]:
+
+parameters = {'styleid': styleid, 'zip': zip_code, 'fmt' : 'json', 'api_key': key}
+typical_data = requests.get("https://api.edmunds.com/v1/api/tmv/tmvservice/calculatetypicallyequippedusedtmv", params=parameters).json()
+certified_data = requests.get("https://api.edmunds.com/v1/api/tmv/tmvservice/findcertifiedpriceforstyle", params=parameters)
+
+
+# In[119]:
+
+print(typical_data['tmv']['totalWithOptions']['usedPrivateParty'])
+print(typical_data['tmv']['totalWithOptions']['usedTradeIn'])
+print(certified_data.content)
+
 
 # In[ ]:
 
